77d6578b3ed5ce67ce026a8558bfa7ac9fc42fc5
Antriksha Rattan <antriksharattan314@gmail.com>
2024-02-27 19:02:09 +0530

    Popmotion

--- with-swiper/js/index.js
+++ with-swiper/js/index.js
@@ -10,3 +10,26 @@ new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// nav
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector(".brand");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(e => {
+  e.preventDefault();
+  pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, "mouseup touchend").start(() => {
+  spring({
+    from: ballXY.get(),
+    velocity: ballXY.getVelocity(),
+    to: { x: 0, y: 0 },
+    stiffness: 200
+    // mass: 1,
+    // damping: 10
+  }).start(ballXY);
+});

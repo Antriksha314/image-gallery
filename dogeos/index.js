78c7aef970b8627413173b29dd76309ab5a2c2a1
Antriksha Rattan <antriksharattan314@gmail.com>
2024-02-29 08:57:36 +0530

    Dogeos

---
+++ dogeos/index.js
@@ -0,0 +1,17 @@
+const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
+
+function addDoggo() {
+  fetch(BREEDS_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const img = document.createElement("img");
+      img.src = data.message;
+      img.alt = "cute dogoo";
+
+      document.querySelector(".doggos").appendChild(img);
+    });
+}
+
+document.querySelector(".add-doggos").addEventListener("click", addDoggo);
